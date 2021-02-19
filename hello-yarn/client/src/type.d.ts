f5edd7a6655f60d1e0c892f2f038b74f199bd671
James Lucktaylor <jlucktay+github@gmail.com>
2021-02-19 19:12:14 +0000

    style: sort JSON and run Prettier

--- hello-yarn/client/src/type.d.ts
+++ hello-yarn/client/src/type.d.ts
@@ -1,19 +1,19 @@
 interface ITodo {
-  _id: string
-  name: string
-  description: string
-  status: boolean
-  createdAt?: string
-  updatedAt?: string
+  _id: string;
+  name: string;
+  description: string;
+  status: boolean;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 interface TodoProps {
-  todo: ITodo
+  todo: ITodo;
 }
 
 type ApiDataType = {
-  message: string
-  status: string
-  todos: ITodo[]
-  todo?: ITodo
-}
+  message: string;
+  status: string;
+  todos: ITodo[];
+  todo?: ITodo;
+};
