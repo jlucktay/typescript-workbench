1adc86a5f23f58ae7f57a753ebcfce9096f3bfab
James Lucktaylor <jlucktay+github@gmail.com>
2024-01-04 15:39:05 +0000

    feat(google-cloud): add Google API types for TypeScript

--- google-cloud/hello.ts
+++ google-cloud/hello.ts
@@ -1,5 +1,5 @@
 // Imports the Google Cloud client library
-import { ProjectsClient } from '@google-cloud/resource-manager';
+import { ProjectsClient } from "@google-cloud/resource-manager";
 
 // Creates a client
 const client = new ProjectsClient();
@@ -8,7 +8,7 @@ async function quickstart() {
   // Lists current projects
   const projects = client.searchProjectsAsync();
 
-  console.log('Projects:');
+  console.log("Projects:");
 
   for await (const project of projects) {
     console.info(project);
